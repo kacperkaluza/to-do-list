8df1167f995fd333d2271b0dc4ad13405133bf65
Kacper Kaluza <97208353+kacperkaluza@users.noreply.github.com>
2022-05-01 19:12:30 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
+const cnt = document.getElementById("container");
+
 function addTask() {
-    let text = document.getElementById(inputTaskText);
-    
+    const tasktext= document.getElementById("textInput").value;
+    const el = document.createElement("div"); 
+        el.classList.add("task");
+        const p = document.createElement("p");
+            p.innerText = tasktext;
+        el.append(p);
+        const btnDone = document.createElement("button");
+            btnDone.innerHTML = '<img src="img/done.svg">';
+        el.appendChild(btnDone);
+        const btnDelete = document.createElement("button");
+            btnDelete.innerHTML = '<img src="img/delete.svg">';
+        el.appendChild(btnDelete);
+        
+        const btnEdit = document.createElement("button");
+            btnEdit.innerHTML = '<img src="img/edit.svg">';
+        el.appendChild(btnEdit);
+    cnt.append(el);
+};
+
+function deleteTask() {
+    el.remove();
 }
